ffd316a58a274196ef05d624dfd9cc18d970c482
joao <joaovic2012@hotmail.com>
2023-07-31 15:05:00 -0300

    enuns OrderStatus and TypeOfPayment created

---
+++ src/main/java/com/jvmeneses/movie/rental/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.jvmeneses.movie.rental.entities;
+
+public enum OrderStatus {
+    WAINTING_PAYMENT, PAID, CANCELED, RENTED, LATE, RETURNED ;
+}
---
+++ src/main/java/com/jvmeneses/movie/rental/entities/TypeOfPayment.java
@@ -0,0 +1,5 @@
+package com.jvmeneses.movie.rental.entities;
+
+public enum TypeOfPayment {
+    CASH_PAYMENT, CREDIT_CARD, DEBIT_CARD, PIX;
+}
